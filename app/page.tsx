4526c8b22b312188e88e98588b421840d820d333
yassine elkabaili <yassine.elkabaili@gmail.com>
2025-01-08 20:03:57 -0500

    hellow world

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <div>
-          <h1>Hello World</h1>
-        </div>
+        <div>Hello World !!</div>
       </main>
   );
 }
